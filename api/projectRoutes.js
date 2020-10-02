b797a683861c00a99604662569834347ef27c701
William Herman <williamajherman@gmail.com>
2020-10-02 12:32:19 -0400

    project router mostly complete

--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -1,8 +1,44 @@
 const express = require('express');
+const db = require('../data/helpers/projectModel');
+
 const projectRouter = express.Router();
 
-projectRouter.get('/api/projects', (res, req) => {
-
+projectRouter.get('/', async (req, res) => {
+    try {
+        const projects = await db.get();
+        res.status(200).json({data: projects});
+    } catch {
+        res.status(500).json({errorMessage: "internal server error"});
+    };
 });
+
+projectRouter.get('/:id', async (req, res) => {
+    try {
+        const project = await db.get(req.params.id);
+        if (project.length){
+            res.status(200).json({data: project});
+        }
+        else {
+            res.status(404).json({errorMessage: "please provide a valid id"});
+        }
+    } catch {
+        res.status(500).json({errorMessage: "internal server error"});
+    };
+});
+
+projectRouter.post('/', async (req, res) => {
+    try {
+        if (req.body.name && req.body.description && req.body.completed){
+            const newProject = await db.insert(req.body);
+            res.status(201).json({data: newProject});
+        } else {
+            res.status(400).json({errorMessage: "please provide all of the required fields"});
+        };
+    } catch {
+        res.status(500).json({errorMessage: "internal server error"});
+    }
+});
+
+// projectRouter.put()
 
 module.exports = projectRouter;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const projectRouter = require('../api/projectRoutes');
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'this is the root route of the server'});
 });
+
+server.use('/api/projects', projectRouter);
 
 
 
